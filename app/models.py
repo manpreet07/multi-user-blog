ff6c102ae531ffde45c1a633937411d350ff4937
Manpreet Singh <manpreetsingh@Manpreets-MBP.home>
2016-06-03 22:00:14 -0400

    added User model

--- app/models.py
+++ app/models.py
@@ -7,9 +7,16 @@ class Blog(ndb.Model):
 
     title = ndb.StringProperty(required=True)
     blog = ndb.TextProperty(required=True)
-    # creator = ndb.StringProperty()
+    creator = ndb.ReferenceProperty(reference_class=User)
     dateTime = ndb.DateTimeProperty(auto_now_add=True)
 
     @classmethod
     def query_blogs(cls):
         return cls.query().order(-cls.dateTime)
+
+
+class User(ndb.Model):
+
+    username = ndb.StringProperty(required=True)
+    password = ndb.TextProperty(required=True)
+    email = ndb.EmailProperty(required=True)
